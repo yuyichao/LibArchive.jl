cf86c7a88f1b8ea549c286960376ba618dde8728
Yichao Yu <yyc1992@gmail.com>
2015-11-01 13:54:43 -0500

    Try adding Windows support

--- deps/build.jl
+++ deps/build.jl
@@ -9,4 +9,9 @@ libarchive = library_dependency("libarchive", aliases = ["libarchive"])
     provides(AptGet, "libarchive12", libarchive)
 end
 
+@windows_only begin
+    using WinRPM
+    provides(WinRPM.RPM, "libarchive12", libarchive, os=:Windows)
+end
+
 @BinDeps.install Dict(:libarchive => :libarchive)
